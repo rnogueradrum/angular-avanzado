d436d035986547f44291f21d9c2ba205ef1bb25f
Ricardo <rnogueradrum@gmail.com>
2021-09-17 17:26:16 +0200

    commit final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -83,8 +83,6 @@ import { HttpClientModule } from '@angular/common/http';
     }),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
     }),
     HttpClientModule
